bb878640a78e5e5fd038ca101a2cb87569670402
barrrigudo99 <carlosbarrientoslopez@gmail.com>
2025-10-24 19:13:13 +0200

    Tercer commit en mi_rama_nueva

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 modificaciones = {
     "mod_0":"hacer primer commit",
-    "mod_1":"hacer primer commit"
+    "mod_1":"hacer segundo commit",
+    "mod_2":"hacer tercer commit, en una rama deistinmta (mi_rama_nueva) "
+
 
 
 }
